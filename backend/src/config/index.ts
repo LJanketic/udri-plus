61c9d53d4f2fec1bc921b0e138c8ee3eb4c04f71
Luka Janketić <luka.janketic@gmail.com>
2024-05-11 16:17:24 +0200

    small updates

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,17 +1,21 @@
 import joi from 'joi';
 
 import server, { ServerConfig } from './server';
+import database, { DbConfig } from './database';
 
 export interface Config {
   server: ServerConfig;
+  database: DbConfig;
 }
 
 const schema = joi.object({
   server: joi.object(),
+  database: joi.object(),
 });
 
 const createConfig = (env: any) => ({
   server: server(env),
+  database: database(env),
 });
 
 export default (env: any): Config => joi.attempt(createConfig(env), schema);
